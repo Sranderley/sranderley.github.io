e56c457592a83ef71a60c4a1f1a7dd0431a32b01
Sranderley <stan.r.anderson@gmail.com>
2016-09-17 16:38:06 -0500

    s

--- app/app.js
+++ app/app.js
@@ -5,13 +5,28 @@ angular.module('demos')
 
 function config($stateProvider, $urlRouterProvider){
 
-	$urlRouterProvider.otherwise('/demos');
+	$urlRouterProvider.otherwise('/demos/');
 
 	$stateProvider
 
-	.state('home', {
+	.state('demos', {
 		url: '/demos',
 		templateUrl: 'html/demos.html'
+	})
+
+	.state('demos.placeholder', {
+		url: '/',
+		templateUrl: 'html/placeholder.html'
+	})
+
+	.state('demos.rcButton', {
+		url: '/rcButton',
+		templateUrl: 'html/rcButton.html'
+	})
+
+	.state('demos.rcExpandableElement', {
+		url: '/rcExpandableElement',
+		templateUrl: 'html/rcExpandableElement.html'
 	});
 
 }
