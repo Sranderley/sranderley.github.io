67e5805416858ca85301a8cf8e465c00143d1670
Sranderley <stan.r.anderson@gmail.com>
2016-09-17 07:53:59 -0500

    Adds an injection of ui.router and begins testing on mapping to the main page

--- app/app.js
+++ app/app.js
@@ -1,4 +1,20 @@
-angular.module('demos', []);
+angular.module('demos', ['ui.router']);
+
+angular.module('demos')
+	.config(config);
+
+function config($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise('/demos');
+
+	$stateProvider
+
+	.state('home', {
+		url: '/demos',
+		templateUrl: '../html/demos.html'
+	});
+
+}
 
 angular.module('demos')
 	.controller('test', test);
